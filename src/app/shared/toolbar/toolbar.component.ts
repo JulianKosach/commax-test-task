00ce2fbbade3e51f82bda229603ed2b6710fbd0c
Julian Kosach <juliankosach@gmail.com>
2019-05-22 18:55:50 +0300

    hide toolbar on scroll down

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -9,6 +9,8 @@ import { Location } from '@angular/common';
 })
 export class ToolbarComponent implements OnInit {
   routeUrl: string;
+  lastScrolled: number;
+  showToolbar: boolean;
 
   constructor(
     private route: ActivatedRoute,
@@ -18,11 +20,24 @@ export class ToolbarComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.lastScrolled = 0;
+    this.showToolbar = true;
     this.router.events.subscribe((val) => {
       if (val instanceof NavigationEnd) {
         this.routeUrl = val.url;
       }
     });
+  }
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event) {
+    const scrollTop = event.target.scrollingElement.scrollTop;
+    if (this.lastScrolled > scrollTop || scrollTop < 60) {
+      this.showToolbar = true;
+    } else {
+      this.showToolbar = false;
+    }
+    this.lastScrolled = scrollTop;
   }
 
   goBack(): void {
